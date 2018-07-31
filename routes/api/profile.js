78ef8b96a7494eb3adaae8acf5b66915e63f5bdd
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-07-31 11:10:57 +0545

    get request handler for profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const passport = require('passport');
 
 router.get('/test', (req, res)=>{
     res.json({test: 'Profile route work'})
+})
+
+// @@ GET api/profilr
+// @@ desc Get current user's profile
+// @@ access Private
+router.get('/',passport.authenticate('jwt', { session: false }), (req, res)=>{
+    let errors = {};
+    Profile.findOne({user: req.id})
+        .then(profile=>{
+            if(!profile){
+                errors.noprofile = 'There is no profile for this user'
+                return res.status(404).json(errors)
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err))
 })
 
 module.exports = router;
