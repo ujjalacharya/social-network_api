fbc35ccd98b96bcd64feebb9d6fb2b2bdcf4f15e
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-08-10 17:34:58 +0545

    creates post route for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,30 @@
 const router = require('express').Router();
+const Post = require('../../models/Post');
+const passport = require('passport');
+const validatePostInput = require('../../validation/post')
 
 router.get('/test', (req, res)=>{
     res.json({test: 'Posts route work'})
 })
 
+// @@ POST api/posts/register
+// @@ desc Register POsts
+// @@ access Private
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res)=>{
+
+    const {errors, isValid} = validatePostInput(req.body);
+    if(!isValid){
+       return res.status(400).json(errors)
+    }
+
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+    });
+
+    newPost.save().then((post)=> {res.status(400).json(post)})
+})
 
 module.exports = router;
